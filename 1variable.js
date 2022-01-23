ce2f4370b60c6302b48f234cf0d25beebc309ac5
UsagiB4 <binaryBunny@tutanota.com>
2022-01-23 22:04:44 +0600

    renamed

---
+++ 1variable.js
@@ -0,0 +1,6 @@
+var newVar = 12;  // this is a numeric variable
+var new_name = "Smith";  // string type variable
+var alph = 'a';  // also string
+var pi = 3.1416; // float type variable
+var bol = true; // boolean type variable
+console.log(typeof(new_name));
