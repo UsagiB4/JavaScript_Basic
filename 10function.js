5831311642baeb76ab3bb7c9b09de84ad1a091af
UsagiB4 <binaryBunny@tutanota.com>
2022-01-27 00:53:56 +0600

    Function

---
+++ 10function.js
@@ -0,0 +1,10 @@
+function calculator(num1, num2){
+    var powered = num1 ** num2;
+    var add = num1 + num2;
+    var sub = num1 - num2;
+    var div = num1 / num2;
+    var multi = num1 * num2;
+    console.log(powered, add, sub, div, multi);
+}
+
+calculator(5, 2);
