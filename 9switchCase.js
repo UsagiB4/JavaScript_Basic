0b3edc799c65d0c43ecc50d390e2658371700653
UsagiB4 <binaryBunny@tutanota.com>
2022-01-27 00:01:31 +0600

    Switch Case

---
+++ 9switchCase.js
@@ -0,0 +1,14 @@
+age = 1;
+switch(age){
+    case 12:
+        console.log("you will be a teen next year")
+        break;
+    case 17:
+        console.log("You will be an adult next year,");
+        break;
+    case 18:
+        console.log("You are an adult now.");
+        break;
+    default:
+        console.log("Default text.")
+}
