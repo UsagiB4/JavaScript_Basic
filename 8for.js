432dccf8c538565180438385c383fd431e0d820a
UsagiB4 <binaryBunny@tutanota.com>
2022-01-26 23:54:18 +0600

    For Loop

--- 8for.js
+++ 8for.js
@@ -1,3 +1,8 @@
 for(var count = 0; count < 20; count++ ){
     console.log(count);
 }
+
+var names = ['apple', 'charry', 'tomato', 'berry', 'lemon', 'fig', 'banana'];
+for (var i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
