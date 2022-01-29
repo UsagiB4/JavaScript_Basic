a614a1605bb9ea893a66f68a454ba133bc4cfed0
UsagiB4 <binaryBunny@tutanota.com>
2022-01-29 15:32:23 +0600

    Let and Const

---
+++ 1x2variable.js
@@ -0,0 +1,9 @@
+// variable can be declared using let and const.
+// :::: Let ::::
+let name_1 = "Alice";
+console.log(name_1);
+
+// :::: const ::::
+// const used to declare constant variables
+const unChangedVal = 3.14159;
+console.log(unChangedVal);
