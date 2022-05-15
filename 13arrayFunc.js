d95c9ba19cc8930670bca6d369d05fb952d8bf36
UsagiB4 <binaryBunny@tutanota.com>
2022-05-15 22:56:07 +0600

    array function

---
+++ 13arrayFunc.js
@@ -0,0 +1,6 @@
+const multiNum = num => num*2;  // this is a array function
+const addIt = (num1, num2) => num1 +num2;  // array is defined with '=>'
+const result = multiNum(3);
+console.log(result);
+console.log(addIt(1,4))
+console.log(nam);
