2693d5c7710b777101700fca6a1fe084184b2975
UsagiB4 <binaryBunny@tutanota.com>
2022-05-17 21:53:38 +0600

    new way to define arrow

--- 13arrayFunc.js
+++ 13arrayFunc.js
@@ -1,6 +1,12 @@
 const multiNum = num => num*2;  // this is a array function
 const addIt = (num1, num2) => num1 +num2;  // array is defined with '=>'
+let multply = function(a, b){  //another way to define an arrow function.
+    return a * b
+}
+
+
 const result = multiNum(3);
 console.log(result);
-console.log(addIt(1,4))
-console.log(nam);
+console.log(addIt(1,4));
+console.log(result);
+console.log(multply(2,3));
