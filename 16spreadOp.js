6118b92485586ab3f8de60c71c0462453ef9dc63
UsagiB4 <binaryBunny@tutanota.com>
2022-05-26 05:25:24 +0600

    Spread Operators

---
+++ 16spreadOp.js
@@ -0,0 +1,9 @@
+const numbs = [343, 12, 5655, 122, 787, 3494, 1]
+const numbs2 = [2, 4, 21, 6, 8, 3, 5, 9, 1, 7]
+
+const allNums = [...numbs, ...numbs2]
+console.log(allNums)
+
+const someNumbers = [223, 671, 714, 911]
+const maxVal = Math.max(...someNumbers);
+console.log(maxVal)
