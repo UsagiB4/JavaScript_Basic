57b1c1e825b257b072efe65f1bfeb18db7448715
UsagiB4 <binaryBunny@tutanota.com>
2022-05-26 05:09:24 +0600

    Template string and multiline string

---
+++ 15templateStr.js
@@ -0,0 +1,9 @@
+const name1 = "James";
+const name2 = "June";
+const combName = `${name1} ${name2} is combined`;
+console.log(combName);
+
+const mLine = `This is a 
+multiline
+text`;
+console.log(mLine);
