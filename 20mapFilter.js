b4aafdea9bd47c208b41111204b0b8fb467816ed
UsagiB4 <binaryBunny@tutanota.com>
2022-06-06 15:02:09 +0600

    .map() and .filter()

--- 20mapFilter.js
+++ 20mapFilter.js
@@ -1,8 +1,14 @@
 const nums = [1, 3 ,53, 2, 4, 11, 30];
 let output =[];
-//__________________ MAP _________________
+//__________________ .map() _________________
+// map has 3 main parameters which are 
+// 1. element of an array (first param)
+// 2. index number(s) (second param)
+// 3. the entire array (last param). Returns the whole array
 nums.map(function(element, index, array){
     output.push(element)
+    console.log(` indexes => ${index}`);
+    console.log(` array => ${array}`)
     //return output
 })
 
